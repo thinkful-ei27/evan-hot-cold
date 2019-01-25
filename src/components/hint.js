1ff152bff910513c5e9eefd8d6f2467a412a3213
Publikwerker <jason.michael.hoffman@gmail.com>
2019-01-25 14:27:40 -0800

    change banner color based on guess

--- src/components/hint.js
+++ src/components/hint.js
@@ -2,8 +2,22 @@ import React from 'react';
 import './hint.css';
 
 function Hint(props) {
+  console.log(props.hint);
+  const styles = {backgroundColor: ''};
+  if (props.hint ==='Cold') {
+    styles.backgroundColor= 'blue';
+  }
+  if (props.hint ==='Hot') {
+    styles.backgroundColor= 'tomato';
+  }
+  if (props.hint ==='Warm') {
+    styles.backgroundColor= 'pink';
+  }
+  if (props.hint ==='You Win') {
+    styles.backgroundColor= 'green';
+  }
   return (
-    <div className="hint">
+    <div className="hint" style={styles}>
       <h2>{props.hint}</h2>
     </div>
   );
